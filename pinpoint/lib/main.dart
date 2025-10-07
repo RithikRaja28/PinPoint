e4a83242dfacac977b4929036d67397b6ffa6d0e
Rithik Raja S <rithikraja28.rr@gmail.com>
2025-10-07 08:26:31 +0530

    ui main.dart

--- pinpoint/lib/main.dart
+++ pinpoint/lib/main.dart
@@ -1,44 +1,7 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold();
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:pinpoint/screens/create_campaign_screen.dart';
 import 'package:pinpoint/screens/dashboard_screen.dart';
 import 'package:pinpoint/screens/auth_screen.dart';
-import 'server_page.dart';
-import 'client_page.dart';
 
 void main() {
   runApp(const CampaignApp());
@@ -55,51 +18,6 @@ class CampaignApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Socket Hello Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const RoleSelectionPage(),
-    );
-  }
-}
-
-class RoleSelectionPage extends StatelessWidget {
-  const RoleSelectionPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Select Role')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton.icon(
-              icon: const Icon(Icons.cloud),
-              label: const Text('Run as Server'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const ServerPage()),
-                );
-              },
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton.icon(
-              icon: const Icon(Icons.phone_android),
-              label: const Text('Run as Client'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const ClientPage()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
     final theme = ThemeData(
       useMaterial3: true,
       scaffoldBackgroundColor: bg,
@@ -114,11 +32,14 @@ class RoleSelectionPage extends StatelessWidget {
         elevation: 0,
         centerTitle: false,
       ),
-      cardTheme: CardThemeData( elevation: 8, shape: RoundedRectangleBorder( 
-        borderRadius: BorderRadius.circular(16), // ✅ can't be const 
-        ), 
-        color: surface, shadowColor: Colors.black12, 
+      cardTheme: CardThemeData(
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16), // ✅ can't be const
         ),
+        color: surface,
+        shadowColor: Colors.black12,
+      ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           elevation: 6,
